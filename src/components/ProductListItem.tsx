4f37dd2ba18364cc9fe9e1c44d76fc9bd2f07e6f
ShaHar91 <bollachristiano@gmail.com>
2024-05-08 14:17:39 +0200

    Starting on the menu navigation and make a nested navigation as well

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
 import Colors from '@/src/constants/Colors';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable, TouchableOpacity } from 'react-native';
 import { Product } from '../types';
+import { Link } from 'expo-router';
 
 type ProductListItemProps = {
     product: Product
@@ -10,12 +11,14 @@ export const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaw
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
     return (
-        <View style={styles.container}>
-            <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} resizeMode='contain' />
+        <Link href={`/menu/${product.id}`} asChild>
+            <TouchableOpacity style={styles.container}>
+                <Image source={{ uri: product.image || defaultPizzaImage }} style={styles.image} resizeMode='contain' />
 
-            <Text style={styles.title}>{product.name}</Text>
-            <Text style={styles.price}>${product.price}</Text>
-        </View>
+                <Text style={styles.title}>{product.name}</Text>
+                <Text style={styles.price}>${product.price}</Text>
+            </TouchableOpacity>
+        </Link>
     )
 }
 
@@ -26,8 +29,8 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         padding: 10,
         borderRadius: 20,
-        flex:1,
-        maxWidth:'50%'
+        flex: 1,
+        maxWidth: '50%'
     },
     image: {
         width: '100%',
